ddd7742b514d7713731ba1a7b1e6e93eecc41364
Hanyang Xiao <46546073+Hanyang0901@users.noreply.github.com>
2020-03-15 15:43:08 -0700

    Update admin.py

--- apartment/apps/users/admin.py
+++ apartment/apps/users/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import UserProfile
 # Register your models here.
 
-
 class UserProfileAdmin(admin.AdminSite):
     list_display = ('username', 'email', 'mobile', 'is_superuser', 'date_joined')
     search_fields = ('username', 'email', 'mobile',)
