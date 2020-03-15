2a846e7d99ba30a76894db413fad1d20ec1aa697
Hanyang Xiao <46546073+Hanyang0901@users.noreply.github.com>
2020-03-15 15:48:19 -0700

    Update models.py

--- apartment/apps/users/models.py
+++ apartment/apps/users/models.py
@@ -3,16 +3,17 @@ from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.hashers import make_password
 # Create your models here.
 
-
 # userprofile model
 class UserProfile(AbstractUser):
     mobile = models.CharField(max_length=11, null=True, blank=True)
     image = models.ImageField(upload_to="images/%Y/%m", default=u"images/default.jpg", max_length=100, blank = True)
 
+    
     class Meta:
         verbose_name = "user_information"
         verbose_name_plural = verbose_name
 
+        
     def save(self, *args, **kwargs):
         if self.password.find("pbkdf2_sha256$") ==-1:
             self.password = make_password(self.password)
